e8ad731985f409ca8ae3d253241101af19ce2e47
syedzada198 <syedzada19800@gmail.com>
2024-05-08 21:36:37 +0500

    new changes

--- assets/script.js
+++ assets/script.js
@@ -21,8 +21,11 @@ $(document).ready(function() {
         imageRow.append(imageWrapper);
     });
     var accommodation = [
-        "1. <strong>Room:</strong> Experience comfort and convenience in our cozy hostel rooms.",
-        "2. <strong>Wifi:</strong> We're committed to providing our students with top-notch WiFi connectivity for seamless learning experiences."
+        "1. <strong>Hostel Accommodation:</strong> Hostel accommodation comprises budget-friendly lodging options that typically feature dormitory-style rooms with shared facilities such as bathrooms and communal areas. They are popular among backpackers and budget travelers seeking affordable accommodation.",
+        "2. <strong>Resort Accommodation:</strong> Resort accommodation refers to expansive properties offering a wide range of amenities and recreational facilities, such as swimming pools, golf courses, spas, and entertainment options. Resorts are designed to provide guests with a comprehensive vacation experience, often situated in scenic locations like beaches or mountains.",
+        "3. <strong>Bed and Breakfast (B&B) Accommodation:</strong>  Bed and Breakfast accommodation involves staying in a private home or small inn where guests are provided with overnight accommodation and breakfast the following morning. B&Bs offer a personalized experience with hosts often sharing local knowledge and hospitality.",
+        "4. <strong>Wifi:</strong> We're committed to providing our students with top-notch WiFi connectivity for seamless learning experiences.",
+        "5. <strong>Room:</strong> Experience comfort and convenience in our cozy hostel rooms.",
     ];
     var accoP = $(".acco-p");
     $.each(accommodation, function(index, value) {
